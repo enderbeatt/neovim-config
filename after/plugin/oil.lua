1732c70f9549086678423bc2bed08f5afe77ab3f
EnDeRGeaT <enderjay321@gmail.com>
2024-03-16 18:41:22 +0300

    updated oil.nvim

--- after/plugin/oil.lua
+++ after/plugin/oil.lua
@@ -81,15 +81,8 @@ require("oil").setup({
     use_default_keymaps = true,
     view_options = {
         -- Show files and directories that start with "."
-        show_hidden = false,
-        -- This function defines what is considered a "hidden" file
-        is_hidden_file = function(name, bufnr)
-            return vim.startswith(name, ".")
-        end,
-        -- This function defines what will never be shown, even when `show_hidden` is set
-        is_always_hidden = function(name, bufnr)
-            return false
-        end,
+        show_hidden = true,
+        
         -- Sort file names in a more intuitive order for humans. Is less performant,
         -- so you may want to set to false if you work with large directories.
         natural_order = true,
@@ -160,4 +153,5 @@ require("oil").setup({
         border = "rounded",
     },
 })
+
 vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
