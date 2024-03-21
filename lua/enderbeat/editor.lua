c6fb0ab1b5517236f0927264290c834f14d82097
EnDeRGeaT <enderjay321@gmail.com>
2024-03-21 09:46:48 +0300

    changed search

--- lua/enderbeat/editor.lua
+++ lua/enderbeat/editor.lua
@@ -8,10 +8,13 @@ vim.opt.tabstop = 4
 vim.opt.expandtab = true
 vim.opt.autoindent = true
 
-vim.opt.smartcase = true
+vim.opt.smartindent = true
+
 
 vim.opt.hlsearch = false
 vim.opt.incsearch = true
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
 
 vim.opt.splitbelow = true
 vim.opt.splitright = true
