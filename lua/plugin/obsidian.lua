81ed8686a890675cd46600e380351ff05fc160e3
EnDeRBeaT <enderjay321@gmail.com>
2024-10-24 10:28:07 +0300

    remove ft trigger for obsidian.nvim

--- lua/plugin/obsidian.lua
+++ lua/plugin/obsidian.lua
@@ -2,15 +2,16 @@ return {
     "epwalsh/obsidian.nvim",
     version = "*",  -- recommended, use latest release instead of latest commit
     lazy = true,
-    ft = "markdown",
     -- Replace the above line with this if you only want to load obsidian.nvim for markdown files in your vault:
-    -- event = {
-        --   -- If you want to use the home shortcut '~' here you need to call 'vim.fn.expand'.
-        --   -- E.g. "BufReadPre " .. vim.fn.expand "~" .. "/my-vault/*.md"
-        --   -- refer to `:h file-pattern` for more examples
-        --   "BufReadPre path/to/my-vault/*.md",
-        --   "BufNewFile path/to/my-vault/*.md",
-        -- },
+    event = {
+        -- If you want to use the home shortcut '~' here you need to call 'vim.fn.expand'.
+        -- E.g. "BufReadPre " .. vim.fn.expand "~" .. "/my-vault/*.md"
+        -- refer to `:h file-pattern` for more examples
+        "BufReadPre " .. vim.fn.expand "~" .. "/obsidian-vault/*.md",
+        "BufNewFile " .. vim.fn.expand "~" .. "/obsidian-vault/*.md",
+        "BufReadPre E:\\obsidian-vault\\*.md",
+        "BufNewFile E:\\obsidian-vault\\*.md",
+    },
     dependencies = {
         -- Required.
         "nvim-lua/plenary.nvim",
@@ -20,15 +21,15 @@ return {
     opts = {
         workspaces = {
             {
-                name = "windows",
-                path = "E:\\obsidian-vault",
+                name = "linux",
+                path = "~/obsidian-vault",
                 overrides = {
                     notes_subdir = "000-Zettelkasten/not-linked",
                 },
             },
             {
-                name = "linux",
-                path = "~/obsidian-vault",
+                name = "windows",
+                path = "E:\\obsidian-vault",
                 overrides = {
                     notes_subdir = "000-Zettelkasten/not-linked",
                 },
