fdae5fc679c37d0c9837bc8829ba2ff4554517cd
EnDeRBeaT <enderjay321@gmail.com>
2024-12-30 15:40:09 +0300

    change overseer config
    you have no idea what turmoil i went through to add basically nothing

--- lua/plugin/overseer.lua
+++ lua/plugin/overseer.lua
@@ -10,8 +10,9 @@ return {
                   "on_output_summarize",
                   "on_exit_set_status",
                   "on_complete_notify",
-                  { "on_result_diagnostics_quickfix", open = true, close = true},
-                  { "open_output", direction = "horizontal" },
+                  "on_result_diagnostics",
+                  { "on_result_diagnostics_quickfix", open = true},
+                  { "open_output", direction = "horizontal", focus = true}
               },
               -- Tasks from tasks.json use these components
               default_vscode = {
@@ -19,10 +20,12 @@ return {
                   "on_result_diagnostics",
               },
           },
+          templates = {"builtin"}
       })
       vim.keymap.set('n', '<leader>tt', [[:OverseerToggle<cr>]], {desc = '[T]ask [T]oggle'})
       vim.keymap.set('n', '<leader>tn', [[:OverseerBuild<cr>]], {desc = '[T]ask [N]ew'})
-      vim.keymap.set('n', '<leader>tr', [[:OverseerQuickAction restart<cr>]], {desc = '[T]ask [R]un Last'});
-      vim.keymap.set('n', '<leader>ts', [[:OverseerRunCmd<cr>]], {desc = '[T]ask [S]hell'});
+      vim.keymap.set('n', '<leader>tr', [[:OverseerQuickAction restart<cr>]], {desc = '[T]ask [R]estart'});
+      vim.keymap.set('n', '<leader>ts', [[:OverseerRunCmd ]], {desc = '[T]ask [S]hell'});
+      vim.keymap.set('n', '<leader>tq', [[:OverseerQuickAction open output in quickfix<cr>]], {desc = '[T]ask [Q]uickfix'});
   end
 }
