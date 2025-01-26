7bcbfa4c71402b04876cbca81b2d76746c43eb4e
EnDeRBeaT <enderjay321@gmail.com>
2025-01-26 10:18:04 +0300

    update mini.ai

--- lua/plugin/mini.lua
+++ lua/plugin/mini.lua
@@ -1,10 +1,23 @@
 return {
 
     "echasnovski/mini.nvim",
+    dependencies = {
+        "nvim-treesitter/nvim-treesitter",
+        "nvim-treesitter/nvim-treesitter-textobjects",
+    },
     version = false,
 
     config = function()
-        require("mini.ai").setup({})
+        local gen_spec = require('mini.ai').gen_spec.treesitter
+        require("mini.ai").setup({
+            custom_textobjects = {
+                a = gen_spec({ a = '@parameter.outer', i = '@parameter.inner' }),
+            }
+
+        })
         require("mini.surround").setup({})
+        require("mini.operators").setup({
+            exchange = { prefix = "ge" }
+        })
     end
 }
