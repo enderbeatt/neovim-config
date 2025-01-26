322f17a5b81aa540aa41b25365da1d6db15be73f
EnDeRBeaT <enderjay321@gmail.com>
2025-01-26 20:02:57 +0300

    remove <leader>r

--- lua/editor.lua
+++ lua/editor.lua
@@ -27,7 +27,6 @@ vim.opt.showtabline = 0
 vim.opt.signcolumn = "yes"
 
 vim.keymap.set("x", "<leader>p", [["_dP]])
-vim.keymap.set("n", "<leader>r", [[:wa<cr> 1<c-w><c-w>:!rustc "%" -o "%<".exe && "%<".exe < input.txt > output.txt<cr>]])
 vim.keymap.set("n", "]q", [[:cn<cr>]])
 vim.keymap.set("n", "[q", [[:cp<cr>]])
 vim.keymap.set("t", "<esc><esc>", [[<c-\><c-n>]])
