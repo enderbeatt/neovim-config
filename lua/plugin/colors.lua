e9c7eb762b8407d12a209acac122a6c6ea6103c2
EnDeRBeaT <enderjay321@gmail.com>
2025-03-22 09:12:03 +0300

    change onenord.nvim to my fork

--- lua/plugin/colors.lua
+++ lua/plugin/colors.lua
@@ -1,16 +1,12 @@
 return {
     {
-        "rmehri01/onenord.nvim",
+        "EnDeRGeaT/onenord.nvim",
+        -- dir = '~/projects/misc/onenord.nvim',
         lazy = false,
         priority = 1000,
 
         config = function()
             vim.cmd([[color onenord]])
         end
-    },
-    {
-        "loctvl842/monokai-pro.nvim",
-        lazy = false,
-        priority = 1000,
     }
 }
