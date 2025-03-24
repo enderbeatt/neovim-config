4301a12c2df2e8e29d0ba0fb238d199c1ac8d49e
EnDeRBeaT <enderjay321@gmail.com>
2025-03-24 16:03:29 +0300

    disable ftFT for flash.nvim

--- lua/plugin/flash.lua
+++ lua/plugin/flash.lua
@@ -1,7 +1,9 @@
 return {
     "folke/flash.nvim",
     event = "VeryLazy",
-    opts = {},
+    opts = {
+        modes = { char = { enabled = false } },
+    },
     keys = {
         { "<cr>",     mode = { "n", "x", "o" }, function() require("flash").jump() end,              desc = "Flash" },
         { "S",     mode = { "n", "o" }, function() require("flash").treesitter() end,        desc = "Flash Treesitter" },
