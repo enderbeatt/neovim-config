35aa48ce3eb02e931856464dea1a223353b69590
EnDeRBeaT <enderjay321@gmail.com>
2025-03-28 10:17:46 +0300

    switch to blink.cmp

---
+++ lua/plugin/completion.lua
@@ -0,0 +1,42 @@
+return {
+    'saghen/blink.cmp',
+    dependencies = { 'L3MON4D3/LuaSnip' },
+
+    version = '1.*',
+
+    ---@module 'blink.cmp'
+    ---@type blink.cmp.Config
+    opts = {
+        -- 'default' (recommended) for mappings similar to built-in completions (C-y to accept)
+        -- 'super-tab' for mappings similar to vscode (tab to accept)
+        -- 'enter' for enter to accept
+        -- 'none' for no mappings
+        --
+        -- All presets have the following mappings:
+        -- C-space: Open menu or open docs if already open
+        -- C-n/C-p or Up/Down: Select next/previous item
+        -- C-e: Hide menu
+        -- C-k: Toggle signature help (if signature.enabled = true)
+        --
+        -- See :h blink-cmp-config-keymap for defining your own keymap
+        keymap = { preset = 'enter' },
+
+        completion = {
+            documentation = { auto_show = true, auto_show_delay_ms = 100 },
+            menu = { draw = { columns = { { "label", "label_description", gap = 1 }, { "kind_icon", gap = 1, "kind" } } } }
+        },
+        snippets = { preset = "luasnip" },
+        appearance = {
+            use_nvim_cmp_as_default = true,
+        },
+
+        -- Default list of enabled providers defined so that you can extend it
+        -- elsewhere in your config, without redefining it, due to `opts_extend`
+        sources = {
+            default = { 'lsp', 'path', 'snippets', 'buffer' },
+        },
+
+        fuzzy = { implementation = "prefer_rust_with_warning" }
+    },
+    opts_extend = { "sources.default" }
+}
--- lua/plugin/lsp.lua
+++ lua/plugin/lsp.lua
@@ -11,17 +11,14 @@ return {
         dependencies = {
             "williamboman/mason.nvim",
             "williamboman/mason-lspconfig.nvim",
-            "hrsh7th/nvim-cmp",
-            "hrsh7th/cmp-nvim-lsp",
-            'hrsh7th/cmp-buffer',
-            'hrsh7th/cmp-path',
             "L3MON4D3/LuaSnip",
+            "saghen/blink.cmp"
         },
         config = function()
             vim.api.nvim_create_autocmd('LspAttach', {
                 desc = 'LSP actions',
                 callback = function(event)
-                   vim.b[event.buf].show_signs = false
+                    vim.b[event.buf].show_signs = false
                     local opts = {buffer = event.buf}
 
                     vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
@@ -34,11 +31,10 @@ return {
                 end
             })
 
-            local capabilities = require('cmp_nvim_lsp').default_capabilities()
+            local capabilities = require('blink.cmp').get_lsp_capabilities()
 
             require('mason').setup({})
             require('mason-lspconfig').setup({
-                ensure_installed = {'clangd', 'lua_ls'},
                 handlers = {
                     function(server_name)
                         require("lspconfig")[server_name].setup {
@@ -48,100 +44,58 @@ return {
                 }
             })
 
-            local cmp = require('cmp')
-            local cmp_select = {behavior = cmp.SelectBehavior.Select}
 
-            cmp.setup({
-                sources = {
-                    {name = 'path'},
-                    {name = 'nvim_lsp'},
-                    {name = 'nvim_lua'},
-                    {name = 'luasnip', keyword_length = 2},
-                    {name = 'buffer', keyword_length = 3},
+            vim.diagnostic.config({virtual_text = true})
+
+            require('lspconfig').clangd.setup({
+                capabilities = capabilities,
+                cmd = {
+                    "clangd",
+                    "--background-index",
+                    -- by default, clang-tidy use -checks=clang-diagnostic-*,clang-analyzer-*
+                    -- to add more checks, create .clang-tidy file in the root directory
+                    -- and add Checks key, see https://clang.llvm.org/extra/clang-tidy/
+                    "--clang-tidy",
+                    "--completion-style=bundled",
+                    "--cross-file-rename",
+                    "--header-insertion=never",
                 },
-                mapping = cmp.mapping.preset.insert({
-                    ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
-                    ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
-                    ['<C-Space>'] = cmp.mapping.confirm({ select = true }),
-                    ['<CR>'] = cmp.mapping.confirm({ select = true }),
-                    ['<C-y>'] = cmp.mapping.complete(),
-                    ["<Tab>"] = cmp.mapping(function(fallback)
-                        if cmp.visible() then
-                            cmp.select_next_item()
-                        elseif require("luasnip").expand_or_jumpable() then
-                            require("luasnip").expand_or_jump()
-                        else
-                            fallback()
-                        end
-                    end, { "i", "s" }),
-                    ["<S-Tab>"] = cmp.mapping(function(fallback)
-                        if cmp.visible() then
-                            cmp.select_prev_item()
-                        elseif require("luasnip").jumpable(-1) then
-                            require("luasnip").jump(-1)
-                        else
-                            fallback()
-                        end
-                    end, { "i", "s" }),
-            }),
-            snippet = {
-                expand = function(args)
-                    require('luasnip').lsp_expand(args.body)
-                end,
-            },
-        })
-
-        vim.diagnostic.config({virtual_text = true})
-
-        require('lspconfig').clangd.setup({
-            capabilities = capabilities,
-            cmd = {
-                "clangd",
-                "--background-index",
-                -- by default, clang-tidy use -checks=clang-diagnostic-*,clang-analyzer-*
-                -- to add more checks, create .clang-tidy file in the root directory
-                -- and add Checks key, see https://clang.llvm.org/extra/clang-tidy/
-                "--clang-tidy",
-                "--completion-style=bundled",
-                "--cross-file-rename",
-                "--header-insertion=never",
-            },
-        })
-        require('lspconfig').rust_analyzer.setup({
-            capabilities = capabilities,
-            settings = {
-                ["rust-analyzer"] = {
-                    checkOnSave = true,
-                    check = {
-                        enable = true,
-                        command = "clippy",
-                        features = "all",
+            })
+            require('lspconfig').rust_analyzer.setup({
+                capabilities = capabilities,
+                settings = {
+                    ["rust-analyzer"] = {
+                        checkOnSave = true,
+                        check = {
+                            enable = true,
+                            command = "clippy",
+                            features = "all",
+                        },
                     },
-                },
-            }
-        })
-        require('lspconfig').basedpyright.setup({
-            capabilities = capabilities,
-            settings = {
-                basedpyright = {
-                    analysis = {
-                        typeCheckingMode = "basic",
+                }
+            })
+            require('lspconfig').basedpyright.setup({
+                capabilities = capabilities,
+                settings = {
+                    basedpyright = {
+                        analysis = {
+                            typeCheckingMode = "basic",
+                        }
                     }
                 }
-            }
-        })
-        require('lspconfig').lua_ls.setup({
-            capabilities = capabilities,
-            settings = {
-                Lua = {
-                    runtime = { version = "Lua 5.1" },
-                    diagnostics = {
-                        globals = { "vim", "it", "describe", "before_each", "after_each" },
+            })
+            require('lspconfig').lua_ls.setup({
+                capabilities = capabilities,
+                settings = {
+                    Lua = {
+                        runtime = { version = "Lua 5.1" },
+                        diagnostics = {
+                            globals = { "vim", "it", "describe", "before_each", "after_each" },
+                        }
                     }
                 }
-            }
-        })
+            })
 
-    end
-}
+        end
+    }
 }
