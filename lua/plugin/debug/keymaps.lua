1484a5cc6808f2aae0a562b2b5db398c3a02aec2
EnDeRBeaT <enderjay321@gmail.com>
2025-05-01 17:30:41 +0300

    separate ui and keymaps

--- lua/plugin/debug/keymaps.lua
+++ lua/plugin/debug/keymaps.lua
@@ -21,91 +21,13 @@ end
 return {
     "nvimtools/hydra.nvim",
     dependencies = {
-        'mfussenegger/nvim-dap',
         "rcarriga/nvim-dap-ui",
-        "nvim-neotest/nvim-nio",
-        'theHamsta/nvim-dap-virtual-text',
     },
     config = function ()
         local hydra = require('hydra')
         local dap = require('dap')
-
         local dapui = require("dapui")
-        dapui.setup({
-            controls = {
-                element = "repl",
-                enabled = true,
-                icons = {
-                    disconnect = "",
-                    pause = "",
-                    play = "",
-                    run_last = "",
-                    step_back = "",
-                    step_into = "",
-                    step_out = "",
-                    step_over = "",
-                    terminate = ""
-                }
-            },
-            element_mappings = {},
-            expand_lines = true,
-            floating = {
-                border = "single",
-                mappings = {
-                    close = { "<Esc>", "q" }
-                }
-            },
-            force_buffers = true,
-            icons = {
-                collapsed = "",
-                current_frame = "",
-                expanded = ""
-            },
-            layouts = { {
-                elements = { {
-                    id = "watches",
-                    size = 0.25
-                }, {
-                        id = "scopes",
-                        size = 0.25
-                    }, {
-                        id = "breakpoints",
-                        size = 0.25
-                    }, {
-                        id = "stacks",
-                        size = 0.25
-                    } },
-                position = "right",
-                size = 60
-            }, {
-                    elements = { {
-                        id = "console",
-                        size = 0.50
-                    }, {
-                            id = "repl",
-                            size = 0.50
-                        } },
-                    position = "bottom",
-                    size = 12
-                } },
-            mappings = {
-                edit = "e",
-                expand = { "<CR>", "<2-LeftMouse>" },
-                open = "o",
-                remove = "d",
-                repl = "r",
-                toggle = "t"
-            },
-            render = {
-                indent = 1,
-                max_value_lines = 100
-            }
-        }
-        )
 
-        dap.set_log_level('trace')
-        -- dap.defaults.fallback.terminal_win_cmd = 'split new'
-        local widgets = require('dap.ui.widgets')
         require('nvim-dap-virtual-text').setup({})
         hydra({
             name = "DEBUG",
@@ -172,18 +94,5 @@ return {
                 { '<Esc>', nil,  { exit = true, desc = false } },
             },
         })
-
-        dap.listeners.before.attach.dapui_config = function()
-            dapui.open()
-        end
-        dap.listeners.before.launch.dapui_config = function()
-            dapui.open()
-        end
-        dap.listeners.before.event_terminated.dapui_config = function()
-            dapui.close()
-        end
-        dap.listeners.before.event_exited.dapui_config = function()
-            dapui.close()
-        end
     end
 }
