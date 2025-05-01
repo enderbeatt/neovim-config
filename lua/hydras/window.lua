f88296ad0a52cf6002a86207c88b94c0a7358877
EnDeRBeaT <enderjay321@gmail.com>
2025-05-01 18:03:08 +0300

    move window hydra out of debug/keymaps

---
+++ lua/hydras/window.lua
@@ -0,0 +1,25 @@
+local hydra = require('hydra')
+
+hydra({
+    name = "WINDOW",
+    mode = "n",
+    body = "<C-W>",
+    heads = {
+        { 'h', '<C-w>h' },
+        { 'j', '<C-w>j' },
+        { 'k', '<C-w>k' },
+        { 'l', '<C-w>l' },
+        { 's', '<C-w>s' },
+        { 'v', '<C-w>v' },
+        { '+', '<C-w>+' },
+        { '-', '<C-w>-' },
+        { '>', '<C-w>>' },
+        { '<', '<C-w><' },
+        { '=', '<C-w>=' },
+        { 'q', '<cmd>close<CR>' },
+        { 'o', '<cmd>only<CR>' },
+        { '_', '<C-w>_' },
+        { '|', '<C-w>|' },
+        { '<Esc>', nil,  { exit = true, desc = false } },
+    },
+})
