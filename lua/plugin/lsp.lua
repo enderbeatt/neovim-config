a51fe3d782dfb67b40eccd23d83c63d5c0c92183
EnDeRBeaT <enderjay321@gmail.com>
2025-07-17 19:04:37 +0300

    change clangd flags

--- lua/plugin/lsp.lua
+++ lua/plugin/lsp.lua
@@ -45,12 +45,14 @@ return {
                 cmd = {
                     "clangd",
                     "--background-index",
+                    "-j=8",
                     -- by default, clang-tidy use -checks=clang-diagnostic-*,clang-analyzer-*
                     -- to add more checks, create .clang-tidy file in the root directory
                     -- and add Checks key, see https://clang.llvm.org/extra/clang-tidy/
                     "--clang-tidy",
                     "--completion-style=bundled",
                     "--cross-file-rename",
+                    "--experimental-modules-support",
                     "--header-insertion=never",
                 },
             })
