3dd3128c74c550a08a9a64044107c7171d38ad46
EnDeRBeaT <enderjay321@gmail.com>
2025-09-14 18:53:19 +0300

    add zls to lspconfig

--- lua/plugin/lsp.lua
+++ lua/plugin/lsp.lua
@@ -100,7 +100,13 @@ return {
                     }
                 }
             })
-
+            require('lspconfig').zls.setup({
+                capabilities = capabilities,
+                cmd = { "zls" },
+                filetypes = { "zig", "zir" },
+                root_dir = require('lspconfig').util.root_pattern("zls.json", "build.zig", ".git"),
+                single_file_support = true,
+            })
         end
     }
 }
