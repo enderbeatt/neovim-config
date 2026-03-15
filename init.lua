e7d89223982baaf64b54c79b1f4c2eb0828ed512
enderbeat <enderjay321@gmail.com>
2026-03-15 11:43:47 +0300

    add conjure

--- init.lua
+++ init.lua
@@ -19,6 +19,7 @@
 -- vim.keymap.set("", "<f1>", toggle_profile)
 
 vim.g.mapleader = " "
+vim.g.maplocalleader = " "
 
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
