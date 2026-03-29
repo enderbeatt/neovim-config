b456dff8d9984acfc643bce03c2779d8ab73763b
EnDeRBeaT <enderjay321@gmail.com>
2026-03-29 21:42:35 +0300

    migrate to main branch of treesitter

--- lua/plugin/treesitter.lua
+++ lua/plugin/treesitter.lua
@@ -1,41 +1,28 @@
+vim.api.nvim_create_autocmd({ 'FileType' }, {
+  callback = function(event)
+    local ok, nvim_treesitter = pcall(require, 'nvim-treesitter')
+    if not ok then return end
+
+    local parsers = require('nvim-treesitter.parsers')
+    if not parsers[event.match] or not nvim_treesitter.install then return end
+
+    local ft = vim.bo[event.buf].ft
+    local lang = vim.treesitter.language.get_lang(ft)
+    nvim_treesitter.install({ lang }):await(function(err)
+      if err then
+        vim.notify('Treesitter install error for ft: ' .. ft .. ' err: ' .. err)
+        return
+      end
+
+      pcall(vim.treesitter.start, event.buf)
+      vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+      vim.wo.foldexpr = 'v:lua.vim.treesitter.foldexpr()'
+    end)
+  end,
+})
+
 return {
-
     "nvim-treesitter/nvim-treesitter",
-    build = ":TSUpdate",
-
-    config = function()
-        require'nvim-treesitter.configs'.setup {
-            ensure_installed = { "c", "cpp", "cmake", "python", "lua", "vim", "vimdoc", "query", "markdown", "markdown_inline"},
-
-            -- Install parsers synchronously (only applied to `ensure_installed`)
-            sync_install = false,
-            -- Automatically install missing parsers when entering buffer
-            -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
-            auto_install = true,
-
-            highlight = {
-                enable = true,
-
-                -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
-                -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
-                -- Using this option may slow down your editor, and you may see some duplicate highlights.
-                -- Instead of true it can also be a list of languages
-                additional_vim_regex_highlighting = false,
-            },
-            indent = {
-                enable = true
-            },
-            -- incremental_selection = {
-            --     enable = true,
-            --     keymaps = {
-            --         init_selection = "<M-o>",
-            --         scope_incremental = "<M-O>",
-            --         node_incremental = "<M-o>",
-            --         node_decremental = "<M-i>",
-            --     },
-            -- },
-        }
-
-        vim.treesitter.language.register('c', { 'vs', 'fs' })
-    end
+    lazy = false,
+    build = ":TSUpdate"
 }
