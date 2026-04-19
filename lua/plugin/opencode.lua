43103b8a0a57f56c863c574c54cf743026a16067
enderbeat <enderjay321@gmail.com>
2026-04-19 17:11:03 +0300

    add opencode.nvim

---
+++ lua/plugin/opencode.lua
@@ -0,0 +1,43 @@
+return {
+  "nickjvandyke/opencode.nvim",
+  version = "*", -- Latest stable release
+  dependencies = {
+    {
+      "folke/snacks.nvim",
+      optional = true,
+      opts = {
+        input = {},
+        picker = {
+          actions = {
+            opencode_send = function(...) return require("opencode").snacks_picker_send(...) end,
+          },
+          win = {
+            input = {
+              keys = {
+                ["<a-a>"] = { "opencode_send", mode = { "n", "i" } },
+              },
+            },
+          },
+        },
+      },
+    },
+  },
+  config = function()
+    ---@type opencode.Opts
+    vim.g.opencode_opts = {
+      -- Your configuration, if any; goto definition on the type or field for details
+    }
+
+    vim.o.autoread = true -- Required for `opts.events.reload`
+
+    vim.keymap.set({ "n", "x" }, "<leader>ap", function() require("opencode").ask("@this: ", { submit = true }) end, { desc = "Prompt opencode" })
+    vim.keymap.set({ "n", "x" }, "<leader>ax", function() require("opencode").select() end,                          { desc = "Execute opencode action…" })
+    vim.keymap.set("n",          "<leader>at", function() require("opencode").toggle() end,                          { desc = "Toggle opencode" })
+
+    vim.keymap.set({ "n", "x" }, "go",  function() return require("opencode").operator("@this ") end,        { desc = "Add range to opencode", expr = true })
+    vim.keymap.set("n",          "goo", function() return require("opencode").operator("@this ") .. "_" end, { desc = "Add line to opencode", expr = true })
+
+    vim.keymap.set("n", "<S-C-u>", function() require("opencode").command("session.half.page.up") end,   { desc = "Scroll opencode up" })
+    vim.keymap.set("n", "<S-C-d>", function() require("opencode").command("session.half.page.down") end, { desc = "Scroll opencode down" })
+  end,
+}
